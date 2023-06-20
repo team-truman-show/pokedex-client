50c74151b99d5c5321bb3f1320be1ba28233488f
kim228sy <kim228sy@gmail.com>
2023-06-20 17:37:45 +0900

    update: 메인페이지 CSS

--- front-end/src/styles/listPokemon.style.js
+++ front-end/src/styles/listPokemon.style.js
@@ -1,25 +1,70 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
-  width: 100vw;
-  padding: 20px 100px;
+  padding: 50px 100px;
   display: flex;
   flex-direction: column;
 `;
 
 export const SearchBar = styled.form`
-  background-color: pink;
-  width: 300px;
+  width: 220px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Input = styled.input`
+  height: 25px;
+  text-align: center;
+  border: none;
+  border-bottom: 1px solid #cdcdcd;
+
+  &:focus {
+    outline: none;
+    border-bottom: 2px solid;
+  }
+`;
+
+export const SearchBtn = styled.button`
+  height: 25px;
+  border: 1px solid #cdcdcd;
 `;
 
 export const Container = styled.div`
   width: 100%;
-  max-width: 1000px;
-  margin: 4rem auto;
+  padding: 30px 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  grid-gap: 15px;
 `;
 
-export const PostItem = styled.div`
+export const Img = styled.img`
+  width: 60%;
+  transition: transform 0.3s;
+  // background-color: black;
+`;
+
+// export const PostItem = styled.div`
+export const PostItem = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
   width: 100%;
-  height: 350px;
-  border: 2px solid black;
+  height: 300px;
+  background-color: #f8f8ff;
+  border-radius: 5px;
+  overflow: hidden;
+
+  &:hover ${Img} {
+    transform: scale(1.4);
+  }
+`;
+
+export const Btn = styled.button`
+  border: 1px solid #cdcdcd;
+  font-size: 15px;
+  font-weight: bold;
+  color: #646cff;
 `;
