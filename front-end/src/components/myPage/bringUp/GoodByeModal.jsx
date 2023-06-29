e47d43dc2ccd5fbaaa854e4d4228c84316d7342e
gusdn98 <ah529@naver.com>
2023-06-29 22:05:44 +0900

    add:버튼css추가

--- front-end/src/components/myPage/bringUp/GoodByeModal.jsx
+++ front-end/src/components/myPage/bringUp/GoodByeModal.jsx
@@ -51,13 +51,38 @@ const GoodByeModal = ({
         <Typography id="custom-modal-description">{message}</Typography>
         <Button
           onClick={handleClick}
-          className={isError ? "nes-btn is-error" : "nes-btn is-success"}
+          variant={isError ? "contained" : "outlined"}
+          sx={{
+            border: "2px solid black",
+            marginTop: "1rem",
+            marginRight: "20px",
+            padding: "0.5rem 1rem",
+            backgroundColor: isError ? "#FF0000" : "#00FF00",
+            color: "white",
+            boxShadow: "none",
+            "&:hover": {
+              boxShadow: "none",
+              backgroundColor: isError ? "#CC0000" : "#00CC00",
+            },
+          }}
         >
           놓아주기
         </Button>
         <Button
           onClick={onClose}
-          className={isError ? "nes-btn is-error" : "nes-btn is-success"}
+          variant={isError ? "contained" : "outlined"}
+          sx={{
+            border: "2px solid black",
+            marginTop: "1rem",
+            padding: "0.5rem 1rem",
+            backgroundColor: isError ? "#FF0000" : "#FF0000",
+            color: "#FFFFFF",
+            boxShadow: "none",
+            "&:hover": {
+              boxShadow: "none",
+              backgroundColor: isError ? "#CC0000" : "#CC0000",
+            },
+          }}
         >
           취 소
         </Button>
